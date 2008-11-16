ea6073b6fd58f8ee57f0380d34600a96f55f5d8d
Alan Snelson <Alan@Wave2.org>
2008-11-16 13:33:01 +0000

    AuthGroup move

--- lib/AuthStor/Schema/AuthStorDB.pm
+++ lib/AuthStor/Schema/AuthStorDB.pm
@@ -9,7 +9,7 @@ AuthStorDB -- DBIC Schema Class
 # Our schema needs to inherit from 'DBIx::Class::Schema'
 use base qw/DBIx::Class::Schema/;
 
-__PACKAGE__->load_classes(qw/Auth AuthTag Attachment AuthAtt Group Tag User UserRole UserTag Role/);
+__PACKAGE__->load_classes(qw/Auth AuthTag Attachment AuthAtt AuthGroup Tag User UserRole UserTag Role/);
 
 
 1;
