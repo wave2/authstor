f3b298e8d933799faf4eee9ad1c001910eb50a0e
Raul Lasluisa <raulslasluisa@gmail.com>
2009-01-27 18:02:34 +0000

    set updateserver to Auth controller

--- lib/AuthStor/Controller/Auth.pm
+++ lib/AuthStor/Controller/Auth.pm
@@ -336,9 +336,38 @@ sub add : Local {
 }
 
 
-=head1 AUTHOR
+###########################################
+################Update Server##############
+###########################################
+
+sub updateserver : Regex('^auth(\d+)/updateserver$') {
+    my ( $self, $c ) = @_;
+
+    $c->stash->{expandGroup} = 0;
+    $c->stash->{group} = $c->model('AuthStorDB::AuthGroup')->single({ parent_id => 0 });
+    # Tag Cloud
+    my $cloud = HTML::TagCloud->new(levels=>5);
+    my $tags = $c->model('AuthStorDB::AuthTag')->search({},
+    {
+      join => 'tag',
+      select => [ { count => '*' }, 'tag.tag_text' ],
+      as => [qw/tagcount tag_text/],
+      group_by => [qw/tag_text/]
+    });
+    while( my $tag=$tags->next() ) {
+      $cloud->add(lc($tag->get_column('tag_text')),$c->uri_for('/search/tag', lc($tag->get_column('tag_text'))), $tag->get_column('tagcount'));
+    }
+    $c->stash->{tag_cloud} =  $cloud->html(50);
+    $c->stash->{title} = 'Update Server';
+    $c->stash->{template} = 'updateServer.tt2';
+    $c->forward('AuthStor::View::TT');
+}
+
+
+=head1 Author
 
 Charlie &
+Raul S Lasluisa
 
 =head1 LICENSE
 
